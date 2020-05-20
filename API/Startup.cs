b949a243111c682617f98fc7584a72f889050334
canyener <can.yener@hotmail.com.tr>
2020-05-20 10:01:23 +0300

    Disables https redirection for now

--- API/Startup.cs
+++ API/Startup.cs
@@ -36,7 +36,7 @@ namespace API
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
 
             app.UseRouting();
 
