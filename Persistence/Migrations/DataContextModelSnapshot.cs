62f57debc3036702c7f948fa7989a28d571872e1
canyener <can.yener@hotmail.com.tr>
2020-05-23 15:14:04 +0300

    Updates DataContextModelSnapshot file

--- Persistence/Migrations/DataContextModelSnapshot.cs
+++ Persistence/Migrations/DataContextModelSnapshot.cs
@@ -13,7 +13,7 @@ namespace Persistence.Migrations
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "3.1.2");
+                .HasAnnotation("ProductVersion", "3.1.4");
 
             modelBuilder.Entity("Domain.Value", b =>
                 {
@@ -27,6 +27,23 @@ namespace Persistence.Migrations
                     b.HasKey("Id");
 
                     b.ToTable("Values");
+
+                    b.HasData(
+                        new
+                        {
+                            Id = 1,
+                            Name = "Value 101"
+                        },
+                        new
+                        {
+                            Id = 2,
+                            Name = "Value 102"
+                        },
+                        new
+                        {
+                            Id = 3,
+                            Name = "Value 103"
+                        });
                 });
 #pragma warning restore 612, 618
         }
