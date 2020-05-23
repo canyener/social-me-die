704726e73071036dfd877c68be34b631c2369693
canyener <can.yener@hotmail.com.tr>
2020-05-23 15:13:24 +0300

    Overrides OnModelCreating method

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -3,13 +3,23 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Persistence
 {
-    public class DataContext : DbContext
+  public class DataContext : DbContext
+  {
+    public DataContext(DbContextOptions options) : base(options)
     {
-        public DataContext(DbContextOptions options) : base(options)
-        {
-            
-        }
 
-        public DbSet<Value> Values { get; set; }
     }
+
+    public DbSet<Value> Values { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+      builder.Entity<Value>()
+          .HasData(
+              new Value { Id = 1, Name = "Value 101" },
+              new Value { Id = 2, Name = "Value 102" },
+              new Value { Id = 3, Name = "Value 103" }
+          );
+    }
+  }
 }
