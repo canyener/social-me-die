7829c46d63017d38368adc809de68ee8ca51aabf
canyener <canyener1337@gmail.com>
2020-06-01 21:56:21 +0300

    Adds Activity entity

---
+++ Domain/Activity.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Domain
+{
+  public class Activity
+  {
+    public Guid Id { get; set; }
+   
+    public string Title { get; set; }
+    public string Category { get; set; }
+    public string City { get; set; }
+    public string Venue { get; set; }
+    
+    public DateTime Date { get; set; }
+  }
+}
