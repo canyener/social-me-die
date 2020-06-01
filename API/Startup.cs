826488906a93ff3390524a74d44e89c2351fa164
canyener <canyener1337@gmail.com>
2020-06-01 22:29:48 +0300

    Removes commented redirection code

--- API/Startup.cs
+++ API/Startup.cs
@@ -49,8 +49,6 @@ namespace API
                 app.UseDeveloperExceptionPage();
             }
 
-            //app.UseHttpsRedirection();
-
             app.UseCors("CorsPolicy");
 
             app.UseRouting();
