636857753513c1367eb53ba17b79bd4079de927e
canyener <canyener1337@gmail.com>
2020-06-01 22:08:09 +0300

    Adds Description property to Activity entity

--- Domain/Activity.cs
+++ Domain/Activity.cs
@@ -7,6 +7,7 @@ namespace Domain
     public Guid Id { get; set; }
    
     public string Title { get; set; }
+    public string Description { get; set; }
     public string Category { get; set; }
     public string City { get; set; }
     public string Venue { get; set; }
