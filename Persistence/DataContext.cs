75d367989b2e86c4c7639ef6dd9ad99fb95aef56
canyener <canyener1337@gmail.com>
2020-06-01 21:45:21 +0300

    Renames builder to modelBuilder to match base class name

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -12,14 +12,14 @@ namespace Persistence
 
     public DbSet<Value> Values { get; set; }
 
-    protected override void OnModelCreating(ModelBuilder builder)
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-      builder.Entity<Value>()
-          .HasData(
-              new Value { Id = 1, Name = "Value 101" },
-              new Value { Id = 2, Name = "Value 102" },
-              new Value { Id = 3, Name = "Value 103" }
-          );
+      modelBuilder.Entity<Value>()
+        .HasData(
+          new Value { Id = 1, Name = "Value 101" },
+          new Value { Id = 2, Name = "Value 102" },
+          new Value { Id = 3, Name = "Value 103" }
+        );
     }
   }
 }
