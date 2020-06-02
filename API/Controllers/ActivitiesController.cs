ae0b8c4ee2842a19b4412699eb4cc577129c0a43
canyener <canyener1337@gmail.com>
2020-06-02 21:59:16 +0300

    Adds Details Action

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Application.Activities;
@@ -22,6 +23,12 @@ namespace API.Controllers
     public async Task<ActionResult<List<Activity>>> List()
     {
       return await _mediator.Send(new List.Query());
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Activity>> Details(Guid id)
+    {
+      return await _mediator.Send(new Details.Query { Id = id });
     }
   }
 }
