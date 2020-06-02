94d329394bef403fdfdcf7f08ab04cf7dd4a8826
canyener <canyener1337@gmail.com>
2020-06-02 23:11:09 +0300

    Adds Create Action

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -30,5 +30,11 @@ namespace API.Controllers
     {
       return await _mediator.Send(new Details.Query { Id = id });
     }
+
+    [HttpPost]
+    public async Task<ActionResult<Unit>> Create(Create.Command command)
+    {
+      return await _mediator.Send(command);
+    }
   }
 }
