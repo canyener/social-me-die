8dbac60b3b71ff8f24db70f562e8273c2e798730
canyener <canyener1337@gmail.com>
2020-06-02 21:34:57 +0300

    Adds MediatR as service

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,3 +1,5 @@
+using Application.Activities;
+using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
@@ -38,6 +40,8 @@ namespace API
                     policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
                 });
             });
+
+            services.AddMediatR(typeof(List.Handler).Assembly);
             services.AddControllers();
         }
 
