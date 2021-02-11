1a1f64055a952fadc7a83f8b59a52ab545e2e2eb
canyener <can.yener@hotmail.com.tr>
2021-02-11 10:42:48 +0300

    Adds validation rules for Create and Edit commands

--- src/Application/Activities/Create.cs
+++ src/Application/Activities/Create.cs
@@ -2,6 +2,7 @@ using System;
 using System.Threading;
 using System.Threading.Tasks;
 using Domain;
+using FluentValidation;
 using MediatR;
 using Persistence;
 
@@ -18,6 +19,19 @@ namespace Application.Activities
       public string City { get; set; }
       public string Venue { get; set; }
       public DateTime Date { get; set; }
+    }
+
+    public class CommandValidator : AbstractValidator<Command>
+    {
+      public CommandValidator()
+      {
+        RuleFor(x => x.Title).NotEmpty();
+        RuleFor(x => x.Description).NotEmpty();
+        RuleFor(x => x.Category).NotEmpty();
+        RuleFor(x => x.Date).NotEmpty();
+        RuleFor(x => x.City).NotEmpty();
+        RuleFor(x => x.Venue).NotEmpty();
+      }
     }
 
     public class Handler : IRequestHandler<Command>
@@ -44,7 +58,7 @@ namespace Application.Activities
         _dataContext.Activities.Add(activity);
         var success = await _dataContext.SaveChangesAsync() > 0;
 
-        if(success) return Unit.Value;
+        if (success) return Unit.Value;
 
         throw new Exception("Problem saving changes");
       }
